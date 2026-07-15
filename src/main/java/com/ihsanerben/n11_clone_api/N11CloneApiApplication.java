028c83b3cf8e3ad3a222531941484eceb101acdb
ihsanerben <ihsanerben@gmail.com>
2026-07-15 12:36:09 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ihsanerben/n11_clone_api/N11CloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ihsanerben.n11_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N11CloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N11CloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihsanerben/n11_clone_api/N11CloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihsanerben.n11_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11CloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
